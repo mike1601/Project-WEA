8b78408b81061b4a1a1e32cb986a06f0449903fd
mayank.soni@tryduit.com <mayank.soni@tryduit.com>
2019-03-16 17:01:30 +0530

    update on creating component

---
+++ src/components/body/bodyContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Bodycontent extends Component {
+    render() {
+        return (
+            <div>
+                <h3>hI!!!!!!!!!!!!</h3>
+            </div>
+        );
+    }
+}
+
+export default Bodycontent ;
